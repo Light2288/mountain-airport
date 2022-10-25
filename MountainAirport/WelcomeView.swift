54d9f000464c7907dd64006ee8a78f7e520360ab
Davide Aliti <Davide.Aliti@it.ibm.com>
2022-10-26 01:10:22 +0200

    add navigation

--- MountainAirport/WelcomeView.swift
+++ MountainAirport/WelcomeView.swift
@@ -1,20 +1,42 @@
 import SwiftUI
 
 struct WelcomeView: View {
+  enum Dest: Hashable {
+    case allFlights, lastFlight
+  }
+  
   @StateObject var flightInfo = FlightData()
+  @State private var path: [Dest] = []
+  @StateObject var lastFlightInfo = FlightNavigationInfo()
   
   var body: some View {
-    NavigationView {
+    NavigationStack(path: $path) {
       ZStack(alignment: .topLeading) {
         Image("welcome-background")
           .resizable()
           .frame(height: 250)
         VStack(alignment: .leading) {
-          NavigationLink(destination: FlightStatusBoard()) {
+          NavigationLink(value: Dest.allFlights) {
             WelcomeButtonView(title: "Flight Status", subTitle: "Departure and arrival information")
+          }
+          NavigationLink(value: Dest.lastFlight) {
+            if let id = lastFlightInfo.lastFlightId,
+               let lastFlight = flightInfo.getFlightById(id) {
+              WelcomeButtonView(title: "Last Flight: \(lastFlight.flightName)", subTitle: "Show next flight departing or arriving at airport")
+            }
           }
           Spacer()
         }
+        .navigationDestination(for: Dest.self, destination: {
+          switch $0 {
+          case .allFlights: FlightStatusBoard(flights: flightInfo.getDaysFlights(Date()))
+          case .lastFlight:
+            if let id = lastFlightInfo.lastFlightId,
+               let lastFlight = flightInfo.getFlightById(id) {
+              FlightDetails(flight: lastFlight)
+            }
+          }
+        })
         .font(.title)
         .foregroundColor(.white)
         .padding()
@@ -22,11 +44,12 @@ struct WelcomeView: View {
       .navigationTitle("Mountain Airport")
     }
     .navigationViewStyle(.stack)
+    .environmentObject(lastFlightInfo)
   }
 }
-  
-  struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-      WelcomeView()
-    }
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    WelcomeView()
   }
+}
